5b673a12a40ef99b2f32118aace6a2e531bedfe2
Arno luyckx <A.luyckx@protonmail.com>
2023-02-13 11:26:16 +0100

    JS and Jquery basics done, basic styles applied

--- main.js
+++ main.js
@@ -5,3 +5,84 @@
  * TODO orange 
  */
 
+$('#add-button').click(function(element) {
+  element.preventDefault();
+    var itemName = $("input[placeholder='Name:']").val();
+    var itemPrice = $("input[placeholder='Price:']").val();
+      console.log("add item triggered")
+
+  $("tbody").append(`
+    <tr> 
+      <td class="item"> 
+      ${itemName} 
+      </td> 
+      <td class="price"> 
+      ${itemPrice} 
+      </td>
+      <td class="qty"><input type="number" value="0"></td> 
+      <td class="total"></td>
+      <td><button class="remove">remove</button></td> 
+    </tr>
+  `);
+  $("input[placeholder='Name:']").val("");
+  $("input[placeholder='Price:']").val("");
+})
+
+$(document).ready(function() {
+
+  function updateSubtotal(item) {
+    let input = $(item).find(".qty input");
+    //let price = $(item).find(".price").text();
+    let price = parseFloat($(item).find(".price").text());
+      
+    let subtotal = input.val() * price;
+    console.log(subtotal);
+    $(item).find(".total").text(subtotal.toFixed(2) + "$");
+  }
+
+  function updateTotalPrice() {
+    let totalPrice = 0;
+    let subtotals = $(".total");
+      //console.log(subtotals);
+      console.log(totalPrice);
+
+      subtotals.each(function(index, subtotal) {
+        let value = parseFloat($(subtotal).text());
+        if (!isNaN(value)) {
+          totalPrice += value;
+        }
+      });
+    /*  
+    subtotals.each(function(index, subtotal) {
+      totalPrice += parseFloat($(subtotal).text());
+      console.log(totalPrice);
+    });*/
+  
+    $("#total-price").text(totalPrice.toFixed(2) + "$");
+  }
+
+  $("tr").on("change", updateTotalPrice);
+
+  $(".qty input").on("change", function() {
+    updateSubtotal($(this).closest("tr"));
+    updateTotalPrice();
+  });
+
+  $(document).on("input", ".qty input", function() {
+    let price = parseFloat($(this).closest("tr").find(".price").text());
+    let quantity = parseInt($(this).val());
+    let subtotal = price * quantity;
+  
+    $(this).closest("tr").find(".total").text(subtotal.toFixed(2));
+    updateTotalPrice();
+  });
+
+  $(document).on("click", ".remove", function() {
+    $(this).closest("tr").remove();
+    updateTotalPrice()
+    });
+});
+
+
+
+
